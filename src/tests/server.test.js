c1ba750ff739900787ce18418a16e60ff15949e0
ombaez <ombaez5689@gmail.com>
2021-02-15 03:08:58 -0300

    refactor:styles and clean code

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -3,34 +3,38 @@ const app = require("../../index");
 const request = supertest(app);
 const redis = require("redis-mock");
 
-test("Check Redis connection - Hello World - Key Value", async (done) => {
-  const client = redis.createClient({ host: "redis" });
-  client.set("hello", "world");
-  function callback(err, data) {
-    expect(data).toBe("world");
-  }
-  client.get("hello", callback);
-  done();
+describe("Test connections", () => {
+  test("Check Redis connection - Hello World - Key Value", async (done) => {
+    const client = redis.createClient({ host: "redis" });
+    client.set("hello", "world");
+    function callback(err, data) {
+      expect(data).toBe("world");
+    }
+    client.get("hello", callback);
+    done();
+  });
+
+  it("Api test endpoint must return confirm message", async (done) => {
+    const response = await request.get("/test");
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("pass!");
+    done();
+  });
 });
 
-it("Api test endpoint must return confirm message", async (done) => {
-  const response = await request.get("/test");
-  expect(response.status).toBe(200);
-  expect(response.body.message).toBe("pass!");
-  done();
-});
+describe("Tests data with responses", () => {
+  test("Should response with 404 when not found results", async (done) => {
+    const response = await request.get("/value/noexists");
+    expect(response.status).toBe(404);
+    done();
+  });
 
-test("Should response with 404 when not found results", async (done) => {
-  const response = await request.get("/value/noexists");
-  expect(response.status).toBe(404);
-  done();
-});
-
-test("Should response value", async (done) => {
-  const client = redis.createClient({ host: "redis" });
-  client.set("frame", "js");
-  const response = await request.get("/value/frame");
-  expect(response.status).toBe(200);
-  expect(response.body.value).toBe("js");
-  done();
+  test("Should response value", async (done) => {
+    const client = redis.createClient({ host: "redis" });
+    client.set("frame", "js");
+    const response = await request.get("/value/frame");
+    expect(response.status).toBe(200);
+    expect(response.body.value).toBe("js");
+    done();
+  });
 });
--- src/tests/socket.test.js
+++ src/tests/socket.test.js
@@ -3,33 +3,37 @@ const expect = chai.expect;
 const should = chai.should();
 const io = require("socket.io-client");
 
-test("Connection with sockets must return pong", (done) => {
-  const socket = io("http://localhost:3000");
-  socket.emit("ping", { msg: "ping" });
-  socket.on("pong", (data) => {
-    expect(data.msg).contain("pong");
+describe("Sockets connections test", () => {
+  test("Connection with sockets must return pong", (done) => {
+    const socket = io("http://localhost:3000");
+    socket.emit("ping", { msg: "ping" });
+    socket.on("pong", (data) => {
+      expect(data.msg).contain("pong");
+    });
+    done();
   });
-  done();
+
+  test("No connection must return not exist", (done) => {
+    const socket = io("http://localhost:3001/invalidSocket");
+    socket.emit("ping", { msg: "ping" });
+    socket.on("pong", (data) => {
+      should.not.exist(data);
+    });
+    done();
+  });
 });
 
-test("No connection must return not exist", (done) => {
-  const socket = io("http://localhost:3001/invalidSocket");
-  socket.emit("ping", { msg: "ping" });
-  socket.on("pong", (data) => {
-    should.not.exist(data);
+describe("Socket confirm message test", () => {
+  test("Send values and await for confirm event", (done) => {
+    const socket = io("http://localhost:3000");
+
+    const newKey = "Test";
+    const newValue = "Value";
+
+    socket.emit("messageToServer", { newKey, newValue });
+    socket.on("confirmStorage", (data) => {
+      expect(data.message).contain("Guardado OK");
+    });
+    done();
   });
-  done();
-});
-
-test("Send values and await for confirm event", (done) => {
-  const socket = io("http://localhost:3000");
-
-  const newKey = "Test";
-  const newValue = "Value";
-
-  socket.emit("messageToServer", { newKey, newValue });
-  socket.on("confirmStorage", (data) => {
-    expect(data.message).contain("Guardado OK");
-  });
-  done();
 });
